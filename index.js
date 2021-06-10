61101fd778971469358989e0fb4defd747aa3861
Aziza  iliasova <miacorporation25@gmail.com>
2021-06-10 14:49:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,6 +11,17 @@ const tutorials = [
   'what is JSONP?'
 ];
 
+// const titleCased = () => {
+//   return tutorials
+// }
 const titleCased = () => {
-  return tutorials
-}
+   return tutorials
+ function titleCase(str) {
+   return str.split(' ').map(function(word) {
+     return (word.charAt(0).toUpperCase() + word.slice(1));
+   }).join(' ');
+ }
+
+ function titleCased() { 
+   return tutorials.map(tutorial => titleCase(tutorial));
+ } 
